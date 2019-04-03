de51310dca4c0dfbbfbe527b7ac522f50fcac2e6
kengo0924 <light.y.w.m.y0924@gmail.com>
2019-04-03 20:59:49 +0900

    忘れ物

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/tani-yu/dogleash/cmd"
 import _ "github.com/tani-yu/dogleash/cmd/dashboard"
 import _ "github.com/tani-yu/dogleash/cmd/monitor"
+import _ "github.com/tani-yu/dogleash/cmd/config"
 
 func main() {
 	cmd.Execute()
